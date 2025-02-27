9dfa6cfa8b3649173aae2c385ddf661817660a94
marksikaundi <emmanuelmark117@gmail.com>
2025-02-27 00:21:34 +0000

    🔧 Update Header component to use consistent formatting and adjust AgentPulse size

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,23 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import AgentPulse from './AgentPulse'
+"use client";
+import Link from "next/link";
+import React from "react";
+import AgentPulse from "./AgentPulse";
 
 function Header() {
   return (
-    <header className='sticky top-0 z-50 left-0 ring-0  ' >
+    <header className="sticky top-0 z-50 left-0 ring-0  ">
       <div>
         <div>
-            <div>
-                <Link href='/'>
-                <AgentPulse size='medium' color='blue' />
-                <h1>up.Chat</h1>
-                </Link>
-            </div>
-
-
-
+          <div>
+            <Link className="flex items-center  " href="/">
+              <AgentPulse size="small" color="blue" />
+              <h1>up.Chat</h1>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
