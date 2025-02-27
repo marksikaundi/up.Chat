474e663dba2fac383918c591cce25c9d1e308aae
marksikaundi <emmanuelmark117@gmail.com>
2025-02-27 13:43:07 +0000

    🚀 Add server-side YouTube video analysis function with URL redirection

--- actions/analyseYoutubeVideo.ts
+++ actions/analyseYoutubeVideo.ts
@@ -1,6 +1,24 @@
+"use server";
+
+// import { getVideoIdFromUrl } from "@lib/youtube/getVideoIdFromUrl";
+
+import  { redirect } from "next/navigation";
+
+
+export async function analyseYoutubeVideo(formData: FormData){
+    const url = formData.get("url")?.toString();
+    if(!url) return;
+
+
+    const videoId = "abc"; // getVideoIdFromUrl(url);
+    if(!videoId) return;
+
+    // redirect to the video page
+    redirect(`/video/${videoId}/analysis`);
+}
 
 
 
 
 
-"use server"
+
