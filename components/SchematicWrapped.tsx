643f132a14f7b1129cab6a369ec106c917651c4f
marksikaundi <emmanuelmark117@gmail.com>
2025-02-27 14:45:16 +0000

    🔧 Add user identification logic using useSchematicEvents and useUser hooks in SchematicWrapped component

--- components/SchematicWrapped.tsx
+++ components/SchematicWrapped.tsx
@@ -1,10 +1,41 @@
+"use client";
 
-"use client"
-
+import { useUser } from "@clerk/nextjs";
+import { useSchematicEvents } from "@schematichq/schematic-react";
+import { useEffect } from "react";
 
 const SchematicWrapped = ({ children }: { children: React.ReactNode }) => {
-    return {children};
+  const { identify } = useSchematicEvents();
+  const { user } = useUser();
+
+  useEffect(() => {
+    const userName =
+      user?.username ??
+      user?.fullName ??
+      user?.emailAddresses[0]?.emailAddress ??
+      user?.id;
+
+    if (user?.id) {
+      identify({
+        // COMPANY LEY KEY
+        company: {
+          keys: {
+            id: user.id,
+          },
+          name: userName,
+        },
+
+        // USER LEVEL KEY
+        keys: {
+          id: user.id,
+        },
+
+        name: userName,
+      });
     }
+  }, [user, identify]);
+
+  return { children };
+};
 
 export default SchematicWrapped;
-
