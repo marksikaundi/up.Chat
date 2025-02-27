8d22e7f9cd75a9c1cd00e57a35361e3abbfe58ec
marksikaundi <emmanuelmark117@gmail.com>
2025-02-27 00:07:56 +0000

    🛠️ Add Header component to layout and wrap children in main tag

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <div></div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
