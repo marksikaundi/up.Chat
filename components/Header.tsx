1b40102927f108fceeeb8b5323e10c658490854e
marksikaundi <emmanuelmark117@gmail.com>
2025-02-27 00:11:51 +0000

    🔗 Add sticky header with home link and title

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,20 @@
 "use client"
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
   return (
-    <header>
+    <header className='sticky top-0 z-50 left-0 ring-0  ' >
       <div>
-        <div></div>
+        <div>
+            <div>
+                <Link href='/'><h1>up.Chat</h1>
+                </Link>
+            </div>
+
+
+
+        </div>
       </div>
     </header>
   )
